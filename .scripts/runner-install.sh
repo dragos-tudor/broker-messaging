d77d38d33f7c435861e117563a40a5e89317cbee
Dragos Tudor <dragos.tudor@gmail.com>
2026-09-01 17:44:25 +0000

    fix self-runner location.

--- .scripts/runner-install.sh
+++ .scripts/runner-install.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 
 TOKEN=${1:?missing token}
 
-mkdir -p ../.runner && cd ../.runner
+mkdir -p ./.runner && cd ./.runner
 
 echo download the runner package
 curl -o actions-runner-linux-x64-2.336.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.336.0/actions-runner-linux-x64-2.336.0.tar.gz
