90b0ae4bb84d1c79347de00b0500bb00d9122d83
Dragos Tudor <dragos.tudor@gmail.com>
2026-09-11 15:29:03 +0000

    clearing inbound routing project.

--- Routing.Inbound/Using.cs
+++ Routing.Inbound/Using.cs
@@ -7,12 +7,7 @@ global using Persistence.InboxMessage;
 global using Transport.Envelope;
 global using Pipelines.Inbound;
 global using Persistence.DeadLetterMessage;
-global using Persistence.RetryPlan;
 global using Transport.DeadLetterEnvelope;
-global using static Operations.Inbound.Envelope.EnvelopeStates;
-global using static Operations.Inbound.Inbox.InboxStates;
-global using static Operations.Inbound.DeadLetter.DeadLetterStates;
-global using static Operations.Inbound.DeadLetterEnvelope.DeadLetterEnvelopeStates;
 global using static Pipelines.Inbound.InboundFuncs;
 global using static Routing.Inbound.InboundFuncs;
 
