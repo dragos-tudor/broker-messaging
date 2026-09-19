fff6af72e71d094242e22c0e2b4df71e70f105b6
Dragos Tudor <dragos.tudor@gmail.com>
2026-09-19 17:27:42 +0000

    update testing script with the split slnx's.

--- .scripts/testing.sh
+++ .scripts/testing.sh
@@ -3,7 +3,13 @@ set -e
 CONFIGURATION=${1:-Debug}
 
 cd $WORKSPACE_ROOT
-dotnet test --solution broker-messaging-inboxoutbox.slnx \
+dotnet test --solution messaging.core.slnx \
+  --configuration $CONFIGURATION \
+  --no-restore \
+  --no-build \
+  --verbosity minimal
+
+ dotnet test --solution messaging.kafka.slnx \
   --configuration $CONFIGURATION \
   --no-restore \
   --no-build \
